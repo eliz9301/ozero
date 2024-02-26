650401602285e05ba74964819ef19c9bbe37b7ee
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:44:20 +0600

    Задание готово к проверке

--- ConsoleApplication17/ConsoleApplication17.cpp
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -6,5 +6,7 @@ int main() {
 	scanf("%lf", &x);
 	double y = log10(x);
 	printf("Десятичный логарифм от %.2f в %.2f\n", x, y);
+	y *= 2;
+	printf("Значение у, умноженное на 2: %.2f\n", y);
 	return 0;
 }
