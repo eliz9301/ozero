83278848a1bc4eaedf467f94410c7eb54a48fd85
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:43:31 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication17/ConsoleApplication17.cpp
@@ -0,0 +1,10 @@
+﻿#include <stdio.h>
+#include <math.h>
+int main() {
+	double x;
+	printf("чему равен x: ");
+	scanf("%lf", &x);
+	double y = log10(x);
+	printf("Десятичный логарифм от %.2f в %.2f\n", x, y);
+	return 0;
+}
